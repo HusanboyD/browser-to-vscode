77a24c6d5d1e5e3a1084ef135246afa8cf43f98e
Husanboy Dadaboyev <husanboydadaboyevcorei7@mail.ru>
2026-07-14 15:26:55 +0500

    Implement text selection in browser extension

---
+++ browser-extension/content.js
@@ -0,0 +1,9 @@
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+  if (message.type === "GET_SELECTED_TEXT") {
+    const selectedText = window.getSelection().toString().trim();
+
+    sendResponse({
+      text: selectedText,
+    });
+  }
+});
